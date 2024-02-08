6f7b101f22e7be2595e3eadebc6d937199eac66e
Cleyton <cleyron2012@gmail.com>
2024-02-07 21:18:31 -0300

    first commit

---
+++ curso coder/index.js.js
@@ -0,0 +1,12 @@
+let idade = 25
+let nome = Cleyton
+let sobrenome = Silva
+let anoAtual = 2024
+console.log(anoAtual - idade)
+let idadeInserida = prompt('Insira a sua idade: ')
+
+ 
+
+alert(anoAtual - parseInt(idadeInserida))
+console.log(anoatual - idade)
+
